5de22b63eccc563b1fca0a3cbfea6b6162652171
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-11 15:32:53 +0300

    Initial Commit

---
+++ AnimatedSFTabBar/AnimatedSFTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSFTabBarApp.swift
+//  AnimatedSFTabBar
+//
+//  Created by Yunus Emre Taşdemir on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSFTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
